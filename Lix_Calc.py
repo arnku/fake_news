2b4dbed9eeae7d5dc29e1e44c49ce590853f8993
Foverskov <fover99@gmail.com>
2023-03-23 10:41:14 +0100

    Lix

--- Lix_Calc.py
+++ Lix_Calc.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 
-token_dirs = 'tokens_randomized/rand_token_split_0.csv'
+token_dirs = 'tokens_randomized/'
 list = []
 
 with open(token_dirs,'r') as input_file: 
@@ -11,14 +11,17 @@ with open(token_dirs,'r') as input_file:
         list.append(row)
 
 def calc_lix(list):
-    total_words = 0
-    total_long_words = 0
+    total_words = 0.0
+    total_long_words = 0.0
+    total_lix_list = []
     for i in list[1:]:
+        total_long_words = 0.0
         total_words = len(i.split())
         for j in i.split():
             if len(j) > 6:
                 total_long_words += 1
-    return (total_long_words*100)/total_words
+        total_lix_list.append((total_long_words*100)/total_words)
+    return total_lix_list
 
 def sort_list_by_label(list):
     reliable_list = []
@@ -31,19 +34,35 @@ def sort_list_by_label(list):
     return reliable_list, unreliable_list
 
 def mean_lix(list):
-    total_lix = 0
-    for i in list:
-        total_lix += calc_lix(i)
-    return total_lix/len(list)
+    return sum(list)/len(list)
+    
+def std_lix(l1,l2):
+    return (sum((i - l1)**2 for i in l2)/len(l2))**0.5
 
 reliable_list, unreliable_list = sort_list_by_label(list)
+reliable_lix = calc_lix(reliable_list)
+unreliable_lix = calc_lix(unreliable_list)
 
-print("reliable:", calc_lix(reliable_list))
+""" print("reliable:", calc_lix(reliable_list)) """
 
-reliable_mean_lix = mean_lix(reliable_list)
-unreliable_mean_lix = mean_lix(unreliable_list)
-
+print("_______________________________________________________________________________________________")
+print("")
+print("")
+print("MEAN lix numbers:")
+reliable_mean_lix = mean_lix(reliable_lix)
+unreliable_mean_lix = mean_lix(unreliable_lix)
 print("reliable:", reliable_mean_lix)
 print("unreliable:", unreliable_mean_lix)
+print("____________________________________________________")
+print("")
+print("")
+print("STANDARD DEVIATION lix numbers:")
+reliable_std_lix = std_lix(reliable_mean_lix, reliable_lix)
+unreliable_std_lix = std_lix(unreliable_mean_lix, unreliable_lix)
+print("reliable:", reliable_std_lix)
+print("unreliable:", unreliable_std_lix)
+print("____________________________________________________")
+print("")
+print("")
 
        
